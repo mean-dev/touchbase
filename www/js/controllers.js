9482535146f954e785adeb0580c2767d1e9ea97e
waso@moosetank.com <a.v.krizhanovsky@gmail.com>
2016-05-03 22:09:03 +0300

    Init

---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $state,$http) {
+	$scope.signIn = function() {
+		$state.go('tab.leads');
+
+    /*$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    $http.post('http://devops.touchbase.tools/oauth/v2/', {
+          'client_id': 123123,
+          'client_secret': 123123123123,
+          'grant_type': 'password',
+          'username': 'user',
+          'password': 'password',
+
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          }
+    })
+      .success(function(data) {
+          accessToken = data.access_token;
+          $location.path("/secure");
+      })
+      .error(function(data, status) {
+          alert("ERROR: " + data);
+      });
+      */
+    ///
+
+	};
+})
+
+.controller('LeadCtrl', function($scope, $state, $ionicHistory) {
+
+	$scope.goBack = function() {
+		window.location = '/#/tab/leads';
+	};
+
+});
