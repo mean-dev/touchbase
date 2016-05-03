19b4875620bdc88dff9772166fa19792432be1c7
waso@moosetank.com <a.v.krizhanovsky@gmail.com>
2016-05-04 00:03:03 +0300

    inapp browser

--- www/js/loginservice.js
+++ www/js/loginservice.js
@@ -4,7 +4,7 @@ mainApp.service('LoginService', function (localStorageService, $window, $state) 
   var redirectUri = 'http://localhost:8100/#/tab/leads';
   var authUrl = 'http://devops.touchbase.tools/oauth/v2/authorize';
   var clientId = 'CLIENT_ID';
-  
+
   var url = authUrl+"?client_id="+clientId+"&grant_type=authorization_code&redirect_uri="+redirectUri+"&response_type=code&state=UNIQUE_STATE_STRING";
 
   var loginWindow;
@@ -13,8 +13,12 @@ mainApp.service('LoginService', function (localStorageService, $window, $state) 
   var token;
   return {
     login: function () {
+
+      window.open = cordova.InAppBrowser.open;
+
       loginWindow = $window.open(url, '_blank', 'location=no,toolbar=no');
       loginWindow.addEventListener('loadstart', function (evt) {
+
         parser = $window.document.createElement('a');
         parser.href = evt.url;
         params = parser.search.split('&amp;');
